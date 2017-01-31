762c641dbb33d3f156ce95b722edcfd8fff07d1e
RakeshShastry <rakeshshastry92@gmail.com>
2017-01-31 10:10:20 +0530

    First Commit

---
+++ src/main/java/com/allstate/RideAppApplication.java
@@ -0,0 +1,12 @@
+package com.allstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideAppApplication.class, args);
+	}
+}
